756cd16878b8b6e63708bfabc629c6a3959cba96
Sparkmonster <socereally@gmail.com>
2017-09-19 16:07:07 -0600

    Initial Commit

---
+++ db/migrate/20170918225612_create_sections.rb
@@ -0,0 +1,12 @@
+class CreateSections < ActiveRecord::Migration[5.1]
+  def change
+    create_table :sections do |t|
+      t.integer :sectionNum
+      t.string :room
+      t.references :professor, foreign_key: true
+      t.references :course, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
